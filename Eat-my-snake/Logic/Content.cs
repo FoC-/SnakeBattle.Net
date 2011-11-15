bf7d768f8c1c29d4b8435696862869d978a84243
Nickola Kush <nickola.kush@gmail.com>
2011-11-15 09:19:27 +0200

    Removed BOM Clarify enum content

--- Eat-my-snake/Logic/Content.cs
+++ Eat-my-snake/Logic/Content.cs
@@ -2,13 +2,13 @@ namespace EatMySnake.Core.Logic
 {
     public enum Content
     {
-        OwnHead,
-        EnemyHead,
-        OwnBody,
-        EnemyBody,
-        OwnTail,
-        EnemyTail,
-        Wall,
-        Empty
+        OwnHead = 1,
+        EnemyHead = 2,
+        OwnBody = 3,
+        EnemyBody = 4,
+        OwnTail = 5,
+        EnemyTail = 6,
+        Wall = 7,
+        Empty = 8
     }
 }
