899688a6c7c436add5748a3f6ae9766808d89b34
Nickola Kush <nickola.kush@gmail.com>
2011-11-18 00:59:10 +0200

    Moved Directions from snake Renamed snake and new prototype

---
+++ Eat-my-snake/Logic/Core.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EatMySnake.Core.Snake;
+
+namespace EatMySnake.Core.Logic
+{
+    class Core
+    {
+
+        public Move NextMove(Matrix currentStateOfBattleField)
+        {
+            //Check if movement is possible
+            //if (CheckPossibleMoves(currentStateOfBattleField)[0] == Direction.NoWay)
+            //{
+            //    //Return current position of head
+            //    return new Move();
+            //}
+
+            Snake.Snake snake = new Snake.Snake();
+            
+
+            //After calculation we should return new positon of head
+            //add leangth if any tail is eaten or add new head and delete tail);
+            return new Move();
+        }
+
+        //private List<Direction> CheckPossibleMoves(Matrix currentStateOfBattleField)
+        //{
+        //    List<Direction> directions = new List<Direction>();
+        //    int headX = BodyParts[0].x;
+        //    int headY = BodyParts[0].y;
+        //    if (currentStateOfBattleField.Rows[headX, headY + 1].Content == Content.Empty
+        //        || currentStateOfBattleField.Rows[headX, headY + 1].Content == Content.EnemyTail
+        //        || currentStateOfBattleField.Rows[headX, headY + 1].Content == Content.OwnTail)
+        //        directions.Add(Direction.North);
+        //    if (currentStateOfBattleField.Rows[headX - 1, headY].Content == Content.Empty
+        //        || currentStateOfBattleField.Rows[headX - 1, headY].Content == Content.EnemyTail
+        //        || currentStateOfBattleField.Rows[headX - 1, headY].Content == Content.OwnTail)
+        //        directions.Add(Direction.South);
+        //    if (currentStateOfBattleField.Rows[headX - 1, headY].Content == Content.Empty
+        //        || currentStateOfBattleField.Rows[headX - 1, headY].Content == Content.EnemyTail
+        //        || currentStateOfBattleField.Rows[headX - 1, headY].Content == Content.OwnTail)
+        //        directions.Add(Direction.West);
+        //    if (currentStateOfBattleField.Rows[headX + 1, headY].Content == Content.Empty
+        //        || currentStateOfBattleField.Rows[headX + 1, headY].Content == Content.EnemyTail
+        //        || currentStateOfBattleField.Rows[headX + 1, headY].Content == Content.OwnTail)
+        //        directions.Add(Direction.East);
+        //    if (directions.Count == 0)
+        //    {
+        //        directions.Add(Direction.NoWay);
+        //    }
+        //    return directions;
+        //}
+    }
+}
---
+++ Eat-my-snake/Snake/Snake.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EatMySnake.Core.Logic;
+
+namespace EatMySnake.Core.Snake
+{
+    class Snake
+    {
+        public string Name { get; private set; }
+        public Guid Uid { get; private set; }
+
+        public List<Matrix> BrainModules = new List<Matrix>();
+        LinkedList<Move> BodyParts = new LinkedList<Move>();
+
+        public bool LoadSnake(Guid uid)
+        {
+            Uid = uid;
+            Name = Guid.NewGuid().ToString();
+            return true;
+        }
+
+        public bool SaveSnake()
+        {
+            return true;
+        }
+
+        public void NextMove(Move newHeadPosition)
+        {
+            BodyParts.AddFirst(newHeadPosition);
+            BodyParts.RemoveLast();
+        }
+
+        public void Bite(Move newHeadPosition)
+        {
+            BodyParts.AddFirst(newHeadPosition);
+        }
+
+        public void Bitten()
+        {
+            BodyParts.RemoveLast();
+        }
+    }
+}
