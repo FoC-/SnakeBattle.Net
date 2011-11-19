9f35aea63bfc2f0f6e6ab35ef7f99833df7dd3f5
Anton Vynogradenko <restuta8@gmail.com>
2011-11-19 01:44:08 -0800

    Added Machine.Specifications for writing unit tests in BDD style, added example tests for FoC to understand the difference from SpecFlow.

---
+++ EatMySnake.Specifications/Tests/when_Enter_is_called_with_valid_number.cs
@@ -0,0 +1,20 @@
+﻿using Machine.Specifications;
+
+namespace EatMySnake.Specifications.Tests
+{
+    [Subject(typeof(Calculator))]
+    public class when_Enter_is_called_with_valid_number
+    {
+        Establish context = () =>
+            calculator = new Calculator();
+
+        Because of = () => 
+            calculator.Enter(numberWasEntered);
+
+        It should_return_the_same_number_as_was_entered_after_pressing__Add__ = () =>
+            calculator.PressAdd().ShouldEqual(numberWasEntered);
+
+        private static Calculator calculator;
+        private static int numberWasEntered = 10;
+    }
+}
