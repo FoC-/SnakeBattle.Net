96b238d9c2ec36a7a7e301cba89faeaa49bca214
Anton Vynogradenko <restuta8@gmail.com>
2011-11-20 21:01:43 -0800

    Added SnakeProtorype.cs (class for playing with code and pick best looking one),
    added BattleReplay.feature to show how typical feature could look like in our application

---
+++ Eat-my-snake/Prototypes/SnakePrototype.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EatMySnake.Core.Prototypes
+{
+    class SnakePrototype : ISnake
+    {
+        public int BodyLength
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Bite(SnakePrototype bitedSnake)
+        {
+            //fire event: eventBus.Publish(new SnakeBitedEvent(this, bitedSnake))
+            bitedSnake.LooseTail();
+            this.Enlarge();
+        }
+
+        private void Enlarge()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LooseTail()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /*
+     * snake spawns event like: moved to coordina
+     */
+
+    interface ISnake
+    {
+        int BodyLength { get; }
+    }
+
+
+    class BattlePrototype
+    {
+        public event EventHandler NextTurn;
+
+        public void OnNextTurn(EventArgs e)
+        {
+            EventHandler handler = NextTurn;
+            if (handler != null) handler(this, e);
+        }
+
+
+
+    }
+}
