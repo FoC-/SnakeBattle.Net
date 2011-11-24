b0b7173a2dd4f0e1e70ea0724780b99b8ce7b904
Anton Vynogradenko <restuta8@gmail.com>
2011-11-23 20:19:21 -0800

    Reordered tests folder structure -> moved Shuffle method spec's to one file

---
+++ EatMySnake.Specifications/Tests/Core/Extensions/IEnumerable/ShuffleSpec.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using EatMySnake.Core.Extensions;
+
+namespace EatMySnake.Specifications.Tests.Core.Extensions
+{
+    [Subject("Shuffle extension")]
+    public class when_called
+    {
+        Establish context = () =>
+            enumerable = Enumerable.Range(0, 9);
+
+        Because of = () =>
+            suffledEnumerable = enumerable.Shuffle();
+
+        It should_return_elements_in_random_order = () => 
+            suffledEnumerable.ShouldNotEqual(enumerable);
+
+        private static IEnumerable<int> enumerable;
+        private static IEnumerable<int> suffledEnumerable;
+    }
+
+    [Subject("Shuffle extension")]
+    public class when_called_twice_in_a_row
+    {
+        Establish context = () =>
+        {
+            enumerable = Enumerable.Range(0, 9);
+        };
+
+        Because of = () =>
+        {
+            shuffledEnumerableA = enumerable.Shuffle();
+            shuffledEnumerableB = enumerable.Shuffle();
+        };
+
+        It should_return_different_sets_of_elements = () =>
+        {
+            shuffledEnumerableA.ShouldNotEqual(shuffledEnumerableB);
+        };
+
+
+        private static IEnumerable<int> enumerable;
+        private static IEnumerable<int> shuffledEnumerableA;
+        private static IEnumerable<int> shuffledEnumerableB;
+    }
+}
