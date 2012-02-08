a901bea9dbdf9f08aef9739469f377e3d2fcce38
Anton Vynogradenko <restuta8@gmail.com>
2012-02-08 00:55:54 -0800

    added relative snake movement possible implementation (tricky one =)

--- EatMySnake.Core/Prototypes/BattleField.cs
+++ EatMySnake.Core/Prototypes/BattleField.cs
@@ -5,12 +5,50 @@ using System.Text;
 
 namespace EatMySnake.Core.Prototypes
 {
+
+    /*
+     * x1 y1 x2 y2
+     * 8  8| 8  7| F
+     * 
+     *  
+     * var deltaX = x2 - x1; //0 (0, +1 or -1)
+     * var deltaY = y2 - y1; //-1
+     * 
+     * //combined version will just add or substract "0" in one of the cases for each case
+     *      case F:
+     *          x1 = x1 - deltaX;
+     *          y1 = y1 - deltaY;
+     *      case L:
+     *          x1 = x1 - deltaY;
+     *          y1 = y1 + deltaX;
+     *      case R:
+     *          x1 = x1 + deltaY;
+     *          y1 = y1 - deltaX;
+     * 
+     * if (deltaX != 0)
+     *      case F:
+     *          x1 = x1 - deltaX; //x1-- or x1++
+     *      case L:
+     *          y1 = y1 + deltaX; //y1++ or y--
+     *      case R:
+     *          y1 = y1 - deltaX; //y1-- or y++
+     * else //then deltaY != 0 is true, cos coor1 and coor2 are also direct neighbours
+     *      case F:
+     *          y1 = y1 - deltaY;
+     *      case L:
+     *          x1 = x1 - deltaY;
+     *      case R:
+     *          x1 = x1 + deltaY;
+     * 
+     * */
+
+
     /// <summary>
     /// Represents battle field where snakes are trying to eat each other.
     /// </summary>
     public class BattleField
     {
-        private List<Snake> snakes;
+        private List<BattleSnake> snakes;
         private int[] field;
 
         internal VisibleArea GetVisibleAreaFor(BattleSnake snake)
