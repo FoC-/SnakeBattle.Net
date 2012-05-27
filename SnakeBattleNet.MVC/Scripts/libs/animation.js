6de02e04f9800dec357efff3385e5d1ed74dbb06
FoC- <nickola.kush@gmail.com>
2012-05-28 00:35:40 +0300

    Added ability to preload resources for battlefield from json

--- SnakeBattleNet.MVC/Scripts/libs/animation.js
+++ SnakeBattleNet.MVC/Scripts/libs/animation.js
@@ -1,12 +1,28 @@
-﻿function init(battlefield) {
+﻿function init(battlefield, images) {
     var canvas = document.getElementById("canva");
+    canvas.width = battlefield.field.x * 10;
+    canvas.height = battlefield.field.y * 10;
+    
 
-    var imageObject = new Image();
-    imageObject.onload = function () {
-        animate(canvas, battlefield, imageObject);
+    var imageObjects = new Array();
+    preloadImages(imageObjects, images);
+
+    imageObjects[imageObjects.length - 1].onload = function () {
+        animate(canvas, battlefield, imageObjects);
     };
-    imageObject.src = 'Content/snake.bmp';
 };
+
+function preloadImages(o, images) {
+    //Preload field textures
+    o[0] = new Image();
+    o[0].src = images.field;
+    
+    //Preload snakes textures
+    for (var i = 0; i < images.snakes.length; i++) {
+        o[i+1] = new Image();
+        o[i+1].src = images.snakes[i];
+    } 
+}
 
 function animate(canvas, battlefield, imageObject) {
     var context = canvas.getContext("2d");
@@ -27,10 +43,17 @@ function moveSnakes(battlefield) {
 }
 
 function drawBattlefield(context, battlefield, imageObject) {
+    var elem = 10;
+
+    for (var x = 0; x < battlefield.field.x; x++) {
+        for (var y = 0; y < battlefield.field.y; y++) {
+            context.drawImage(imageObject[0], 10, 0, elem, elem, x*10, y*10, elem, elem);
+        }
+    }
+
     var tail = battlefield.segments[0];
     
-    var elem = 10;
-    context.drawImage(imageObject, 0, 0, elem, elem, tail.x, tail.y, elem, elem);
+    context.drawImage(imageObject[2], 0, 0, elem, elem, tail.x, tail.y, elem, elem);
 }
 
 window.requestAnimFrame = (function (callback) {
