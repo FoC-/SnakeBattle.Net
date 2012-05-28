d51a8929b93b0010368b5ebe0fb6ebbadbaccbd0
FoC- <nickola.kush@gmail.com>
2012-05-29 00:23:20 +0300

    New realization with ajax

--- SnakeBattleNet.MVC/Scripts/libs/animation.js
+++ SnakeBattleNet.MVC/Scripts/libs/animation.js
@@ -1,32 +1,58 @@
-﻿function init(battlefield, images) {
+﻿function startReplay(battleReplay, imageObjects) {
     var canvas = document.getElementById("canva");
-    canvas.width = battlefield.field.x * 10;
-    canvas.height = battlefield.field.y * 10;
-    
+    canvas.width = battleReplay.field.X * 10;
+    canvas.height = battleReplay.field.Y * 10;
 
-    var imageObjects = new Array();
-    preloadImages(imageObjects, images);
-
-    imageObjects[imageObjects.length - 1].onload = function () {
-        animate(canvas, battlefield, imageObjects);
-    };
+    animate(canvas, battleReplay, imageObjects);
 };
+
+function PreloadResources(battleLink, texturesLink) {
+    var imageObjects = new Array();
+    loadTextures(texturesLink, imageObjects);
+    loadBattle(battleLink, imageObjects);
+}
+
+function loadTextures(texturesLink, imageObjects) {
+    $.ajax({
+        url: texturesLink,
+        type: 'POST',
+        dataType: 'json',
+        data: '{id: "ID-javascript" }',
+        contentType: 'application/json',
+        success: function (data) { preloadImages(imageObjects, data); },
+        async: false
+    });
+}
+
+function loadBattle(battleLink, imageObjects) {
+    var battle;
+    $.ajax({
+        url: battleLink,
+        type: 'POST',
+        dataType: 'json',
+        data: '{id: "ID-javascript" }',
+        contentType: 'application/json',
+        success: function (data) { battle = data; },
+        complete: function () { startReplay(battle, imageObjects); },
+        async: false
+    });
+}
 
 function preloadImages(o, images) {
     //Preload field textures
     o[0] = new Image();
     o[0].src = images.field;
-    
+
     //Preload snakes textures
     for (var i = 0; i < images.snakes.length; i++) {
-        o[i+1] = new Image();
-        o[i+1].src = images.snakes[i];
-    } 
+        o[i + 1] = new Image();
+        o[i + 1].src = images.snakes[i];
+    }
 }
 
 function animate(canvas, battlefield, imageObject) {
     var context = canvas.getContext("2d");
-    
+
     // clear
     context.clearRect(0, 0, canvas.width, canvas.height);
     // move
@@ -39,21 +65,21 @@ function animate(canvas, battlefield, imageObject) {
 }
 
 function moveSnakes(battlefield) {
-    battlefield.segments[0].x += 10;
+    battlefield.segments.X += 1;
 }
 
 function drawBattlefield(context, battlefield, imageObject) {
     var elem = 10;
 
-    for (var x = 0; x < battlefield.field.x; x++) {
-        for (var y = 0; y < battlefield.field.y; y++) {
-            context.drawImage(imageObject[0], 10, 0, elem, elem, x*10, y*10, elem, elem);
+    for (var x = 0; x < battlefield.field.X; x++) {
+        for (var y = 0; y < battlefield.field.Y; y++) {
+            context.drawImage(imageObject[0], 10, 0, elem, elem, x * 10, y * 10, elem, elem);
         }
     }
 
-    var tail = battlefield.segments[0];
-    
-    context.drawImage(imageObject[2], 0, 0, elem, elem, tail.x, tail.y, elem, elem);
+    var tail = battlefield.segments;
+
+    context.drawImage(imageObject[2], 0, 0, elem, elem, tail.X, tail.Y, elem, elem);
 }
 
 window.requestAnimFrame = (function (callback) {
