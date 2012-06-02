3901adfb39cdcb642b7747c6410b30f9de2dcf3b
FoC- <nickola.kush@gmail.com>
2012-06-02 13:32:13 +0300

    Part of tests for core are moved to another project

---
+++ SnakeBattleNet.Test.Core/BattleManager/when_call_comparator_with_predifined_chip_antother_tail.cs
@@ -0,0 +1,15 @@
+using Machine.Specifications;
+using SnakeBattleNet.Core.Battlemanager;
+
+namespace SnakeBattleNet.Test.Core
+{
+    [Subject(typeof(Comparator))]
+    class when_call_comparator_with_predifined_chip_antother_tail
+    {
+        private It should_return_move_on_that_row = () =>
+        {
+            
+        };
+
+    }
+}
---
+++ SnakeBattleNet.Test.Core/BattleManager/when_creating_BattleManager_and_number_of_snakes_is_more_then_gates.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Moq;
+using SnakeBattleNet.Core;
+using SnakeBattleNet.Core.Battlefield;
+using SnakeBattleNet.Core.Battlemanager;
+using It = Machine.Specifications.It;
+
+namespace SnakeBattleNet.Test.Core
+{
+    [Subject(typeof(BattleManager))]
+    class when_creating_BattleManager_and_number_of_snakes_is_more_then_gates
+    {
+        Establish context = () =>
+        {
+            int numberOfSnakes = 5;
+            int numberOfGateways = 2;
+
+            snakes = Mock.Of<IList<ISnake>>(_ => _.Count == numberOfSnakes);
+            battleField = Mock.Of<IBattleField>(_ => _.Gateways.Count == numberOfGateways);
+        };
+
+        Because of = () =>
+        {
+            exception = Catch.Exception(() => new BattleManager(battleField, snakes));
+        };
+
+        It should_fail = () =>
+        {
+            exception.ShouldNotBeNull();
+        };
+
+        private static Exception exception;
+        private static IList<ISnake> snakes;
+        private static IBattleField battleField;
+    }
+}
