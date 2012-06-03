e1fe232cf1371d4b8de9aecfe38980dbb3b17388
FoC- <nickola.kush@gmail.com>
2012-06-03 23:30:51 +0300

    Logic for comparator finished.

--- SnakeBattleNet.Core/Battlemanager/Comparator.cs
+++ SnakeBattleNet.Core/Battlemanager/Comparator.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using SnakeBattleNet.Core.Battlefield;
 using SnakeBattleNet.Core.Common;
+using SnakeBattleNet.Core.Snake;
 
 namespace SnakeBattleNet.Core.Battlemanager
 {
@@ -16,50 +19,193 @@ namespace SnakeBattleNet.Core.Battlemanager
 
         public Move MakeDecision()
         {
-            Move snakeHeadPositionOnBattleField = this.snake.GetHeadPosition();
+            Move headPositionOnBattleField = this.snake.GetHeadPosition();
+
+            var moveToNorth = new Move(headPositionOnBattleField.X, headPositionOnBattleField.Y - 1, Direction.North);
+            var moveToSouth = new Move(headPositionOnBattleField.X, headPositionOnBattleField.Y - 1, Direction.North);
+            var moveToWest = new Move(headPositionOnBattleField.X, headPositionOnBattleField.Y - 1, Direction.North);
+            var moveToEast = new Move(headPositionOnBattleField.X, headPositionOnBattleField.Y - 1, Direction.North);
 
             foreach (var brainChip in this.snake.BrainModules)
             {
                 int chipSizeDim = brainChip.Size.X;
-                switch (snakeHeadPositionOnBattleField.direction)
+                var headPositionOnBrainChip = brainChip.GetOwnHead();
+                var color = brainChip.HeadColor;
+
+                IEnumerable<FieldRow> fieldRows;
+                switch (headPositionOnBattleField.direction)
                 {
                     case Direction.North:
                         {
-                            this.battleField.ViewToNorth(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToWest(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToEast(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
+                            fieldRows = this.battleField.ViewToNorth(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToNorth;
+                            fieldRows = this.battleField.ViewToWest(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToWest;
+                            fieldRows = this.battleField.ViewToEast(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToEast;
                         }
                         break;
                     case Direction.West:
                         {
-                            this.battleField.ViewToWest(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToSouth(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToNorth(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
+                            fieldRows = this.battleField.ViewToWest(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToWest;
+                            fieldRows = this.battleField.ViewToSouth(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToSouth;
+                            fieldRows = this.battleField.ViewToNorth(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToNorth;
                         }
                         break;
                     case Direction.East:
                         {
-                            this.battleField.ViewToEast(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToNorth(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToSouth(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
+                            fieldRows = this.battleField.ViewToEast(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToEast;
+                            fieldRows = this.battleField.ViewToNorth(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToNorth;
+                            fieldRows = this.battleField.ViewToSouth(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToSouth;
                         }
                         break;
                     case Direction.South:
                         {
-                            this.battleField.ViewToSouth(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToEast(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
-                            this.battleField.ViewToWest(snakeHeadPositionOnBattleField, brainChip.GetOwnHead(), chipSizeDim);
+                            fieldRows = this.battleField.ViewToSouth(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToSouth;
+                            fieldRows = this.battleField.ViewToEast(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToEast;
+                            fieldRows = this.battleField.ViewToWest(headPositionOnBattleField, headPositionOnBrainChip, chipSizeDim);
+                            if (Compare(fieldRows.ToArray(), brainChip.ToEnumeration().ToArray(), color)) return moveToWest;
                         }
                         break;
                 }
             }
+            return null;
+        }
 
-            /*
-                    if(_chips.andType) all sums of sequences should be accepted
+        private bool Compare(FieldRow[] fieldRows, ChipRow[] chipRows, AOColor color)
+        {
+            bool OrBlue = false;
+            int OrBlueCount = 0;
+            bool OrGreen = false;
+            int OrGreenCount = 0;
 
-                    if(_chips.orType) one of all sums of sequences should be accepted
-                        */
-            throw new NotImplementedException();
+            bool AndGrey = true;
+            int AndGreyCount = 0;
+            bool AndRed = true;
+            int AndRedCount = 0;
+            bool AndBlack = true;
+            int AndBlackCount = 0;
+
+            bool andType = color == AOColor.AndBlack || color == AOColor.AndGrey || color == AOColor.AndRed;
+
+            for (int i = 0; i < fieldRows.Length; i++)
+            {
+                var equal = chipRows[i].Equals(fieldRows[i]);
+                var colorOfRow = chipRows[i].AoColor;
+                #region Switch of logic depended on row color
+
+                switch (colorOfRow)
+                {
+                    case AOColor.OrBlue:
+                        {
+                            if (andType)
+                            {
+                                if (equal)
+                                    OrBlue = true;
+                            }
+                            else
+                            {
+                                if (equal)
+                                    return true;
+                            }
+                            OrBlueCount++;
+                        }
+                        break;
+                    case AOColor.OrGreen:
+                        {
+                            if (andType)
+                            {
+                                if (equal)
+                                    OrGreen = true;
+                            }
+                            else
+                            {
+                                if (equal)
+                                    return true;
+                            }
+                            OrGreenCount++;
+                        }
+                        break;
+                    case AOColor.AndGrey:
+                        {
+                            if (andType)
+                            {
+                                if (!equal)
+                                    return false;
+                            }
+                            else
+                            {
+                                if (!equal)
+                                    AndGrey = false;
+                            }
+                            AndGreyCount++;
+                        }
+                        break;
+                    case AOColor.AndRed:
+                        {
+                            if (andType)
+                            {
+                                if (!equal)
+                                    return false;
+                            }
+                            else
+                            {
+                                if (!equal)
+                                    AndRed = false;
+                            }
+                            AndRedCount++;
+                        }
+                        break;
+                    case AOColor.AndBlack:
+                        {
+                            if (andType)
+                            {
+                                if (!equal)
+                                    return false;
+                            }
+                            else
+                            {
+                                if (!equal)
+                                    AndBlack = false;
+                            }
+                            AndBlackCount++;
+                        }
+                        break;
+                }
+
+                #endregion Switch of logic depended on row color
+            }
+
+            if (andType)
+            {
+                if (
+                    (AndGrey || AndGreyCount == 0) &&
+                    (AndRed || AndRedCount == 0) &&
+                    (AndBlack || AndBlackCount == 0) &&
+                    (OrBlue || OrBlueCount == 0) &&
+                    (OrGreen || OrGreenCount == 0)
+                   ) return true;
+            }
+            else
+            {
+                if (
+                    (AndGrey && AndGreyCount > 0) ||
+                    (AndRed && AndRedCount > 0) ||
+                    (AndBlack && AndBlackCount > 0) ||
+                    (OrBlue && OrBlueCount > 0) ||
+                    (OrGreen && OrGreenCount > 0)
+                   ) return true;
+            }
+
+            return false;
         }
     }
 }
