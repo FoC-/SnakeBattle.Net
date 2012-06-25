c7423fb52bf52a05f38cdc0e4938ee7f5242209c
FoC- <nickola.kush@gmail.com>
2012-06-26 02:04:13 +0300

    Realization for training flow

--- SnakeBattleNet.Web/Controllers/TrainingController.cs
+++ SnakeBattleNet.Web/Controllers/TrainingController.cs
@@ -1,39 +1,69 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Security;
+using SnakeBattleNet.Core.Implementation;
+using SnakeBattleNet.Persistance;
 
 namespace SnakeBattleNet.Web.Controllers
 {
     [Authorize]
     public class TrainingController : Controller
     {
-        public ActionResult Index()
+        private IMongoGateway mongoGateway;
+
+        protected override void Initialize(RequestContext requestContext)
         {
-            var id = GetUserId();
-            return View();
+            mongoGateway = new MongoGateway();
+            base.Initialize(requestContext);
         }
 
-        public ActionResult Snake()
+        public ActionResult Index()
         {
-            var id = GetUserId();
-            return View();
+            string id = GetUserId();
+            int number;
+            IEnumerable<Snake> snakes = mongoGateway.GetByOwnerId(id, out number);
+
+            return number == 0 ? View() : View(snakes);
         }
 
         public ActionResult AddSnake()
         {
-            var id = GetUserId();
-            return View();
+            var userId = GetUserId();
+
+            var snake = new Snake(Guid.NewGuid().ToString(), userId);
+            snake.SetLoses(0);
+            snake.SetWins(0);
+            snake.SetModulesMax(9);
+            snake.SetName("Snake Doe");
+            snake.SetScore(1500);
+            snake.SetVisionRadius(7);
+
+            mongoGateway.AddSnake(snake);
+
+            return RedirectToAction("Index");
         }
 
-        public ActionResult EditSnake()
+        public ActionResult EditSnake(string id)
         {
-            var id = GetUserId();
-            return View();
+            return RedirectToAction("Index", "Edit", new { snakeId = id });
         }
 
-        public ActionResult RemoveSnake()
+        public ActionResult RemoveSnake(string snakeId)
         {
-            var id = GetUserId();
-            return View();
+            var userId = GetUserId();
+            var snake = mongoGateway.GetById(snakeId);
+
+            if (snake != null && snake.OwnerId == userId)
+            {
+                mongoGateway.RemoveSnake(snakeId);
+            }
+            else
+            {
+                ModelState.AddModelError("", "This warrior is not belong to you");
+            }
+            return RedirectToAction("Index");
         }
 
         private string GetUserId()
