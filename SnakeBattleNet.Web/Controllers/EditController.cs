0586e24bfb59bb1a8b03372bcfe5db49af68dd82
FoC- <nickola.kush@gmail.com>
2012-06-26 02:05:05 +0300

    Start implementation of edit snake

--- SnakeBattleNet.Web/Controllers/EditController.cs
+++ SnakeBattleNet.Web/Controllers/EditController.cs
@@ -1,43 +1,58 @@
 ﻿using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Security;
+using SnakeBattleNet.Core.Implementation;
+using SnakeBattleNet.Persistance;
 
 namespace SnakeBattleNet.Web.Controllers
 {
+    [Authorize]
     public class EditController : Controller
     {
-        public ActionResult Index()
+        private IMongoGateway mongoGateway;
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            mongoGateway = new MongoGateway();
+            base.Initialize(requestContext);
+        }
+
+        public ActionResult Index(string snakeId)
+        {
+            var snake = mongoGateway.GetById(snakeId);
+
+            CheckOwner(snake);
+
+            return View(snake);
+        }
+
+        public ActionResult EditName(string snakeId)
+        {
+            var snake = mongoGateway.GetById(snakeId);
+            return View();
+        }
+
+        public ActionResult UploadTexture(string snakeId)
         {
             var id = GetUserId();
             return View();
         }
 
-        public ActionResult EditName()
+        public ActionResult AddChip(string snakeId)
         {
-            var id = GetUserId();
+            var snake = mongoGateway.GetById(snakeId);
             return View();
         }
 
-        public ActionResult UploadTexture()
+        public ActionResult EditChip(string snakeId)
         {
-            var id = GetUserId();
+            var snake = mongoGateway.GetById(snakeId);
             return View();
         }
 
-        public ActionResult AddChip()
+        public ActionResult RemoveChip(string snakeId)
         {
-            var id = GetUserId();
-            return View();
-        }
-
-        public ActionResult EditChip()
-        {
-            var id = GetUserId();
-            return View();
-        }
-
-        public ActionResult RemoveChip()
-        {
-            var id = GetUserId();
+            var snake = mongoGateway.GetById(snakeId);
             return View();
         }
 
@@ -45,6 +60,15 @@ namespace SnakeBattleNet.Web.Controllers
         {
             MembershipUser currentUser = Membership.GetUser(User.Identity.Name, userIsOnline: true);
             return currentUser == null ? null : currentUser.ProviderUserKey.ToString();
+        }
+
+        private void CheckOwner(Snake snake)
+        {
+            var userId = GetUserId();
+            if (snake == null || snake.OwnerId != userId)
+            {
+                ModelState.AddModelError("", "This warrior is not belong to you");
+            }
         }
     }
 }
