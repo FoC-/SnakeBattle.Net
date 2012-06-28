bdf30df7409f4fccc4956959dad42276b136abcc
FoC- <nickola.kush@gmail.com>
2012-06-28 15:25:48 +0300

    View models refactored and added new

--- SnakeBattleNet.Web/Models/SnakeViewModelBase.cs
+++ SnakeBattleNet.Web/Models/SnakeViewModelBase.cs
@@ -1,13 +1,20 @@
-﻿namespace SnakeBattleNet.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SnakeBattleNet.Web.Models
 {
     public class SnakeViewModelBase
     {
         public string Id { get; set; }
 
+        [Display(Name = "Snake name")]
+        [StringLength(20, ErrorMessage = "Maximum length for name is 20 symbols")]
+        public string Name { get; set; }
+
         public SnakeViewModelBase() { }
-        public SnakeViewModelBase(string id)
+        public SnakeViewModelBase(string id, string name)
         {
             Id = id;
+            Name = name;
         }
     }
 }
