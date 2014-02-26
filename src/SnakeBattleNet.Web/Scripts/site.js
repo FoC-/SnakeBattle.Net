03dea925c54ef95f4463b8ec8adaae33ad100a59
Nikolay Kush <nickola.kush@gmail.com>
2014-02-26 16:18:14 +0200

    Web: snake edit - selector extended

--- src/SnakeBattleNet.Web/Scripts/site.js
+++ src/SnakeBattleNet.Web/Scripts/site.js
@@ -1,6 +1,18 @@
 ﻿var SBN = SBN || {};
 SBN.Contract = {
-    colorMap: ['blue', 'green', 'grey', 'red', 'black']
+    colorMap: ['blue', 'green', 'grey', 'red', 'black'],
+    imageMap: {
+        ebody: '/img/eBody.png',
+        ehead: '/img/eHead.png',
+        empty: '/img/empty.png',
+        etail: '/img/eTail.png',
+        gateway: '/img/gateway.png',
+        obody: '/img/oBody.png',
+        ohead: '/img/oHead.png',
+        otail: '/img/oTail.png',
+        wall: '/img/wall.png'
+    },
+    content: ['empty', 'wall', 'head', 'body', 'tail', 'gateway']
 };
 
 SBN.Edit = function (settings, Renderer, SnakeService) {
@@ -49,9 +61,13 @@ SBN.Edit = function (settings, Renderer, SnakeService) {
 };
 
 SBN.Kinetic = {};
+SBN.Kinetic.Configuration = {
+    size: 30, //50
+    fontSize: 18
+};
 SBN.Kinetic.createCell = function (cell) {
-    var size = 30; //50
-    var fontSize = 18;
+    var size = SBN.Kinetic.Configuration.size;
+    var fontSize = SBN.Kinetic.Configuration.fontSize;
     var x = cell.position.x * size;
     var y = cell.position.y * size;
     var group = new Kinetic.Group();
@@ -121,17 +137,18 @@ SBN.Kinetic.renderSelector = function ($container) {
     var model = {};
     $container.data('model', model);
 
-    var size = 30; //50
+    var size = SBN.Kinetic.Configuration.size;
+    var elementNumber = 0;
     var stage = new Kinetic.Stage({
         container: $container[0],
-        width: 350,
-        height: 50
+        width: size * 14,
+        height: size
     });
     var layer = new Kinetic.Layer();
 
     $.each(SBN.Contract.colorMap, function (index, value) {
         var rect = new Kinetic.Rect({
-            x: index * size,
+            x: elementNumber++ * size,
             y: 0,
             width: size,
             height: size,
@@ -140,32 +157,121 @@ SBN.Kinetic.renderSelector = function ($container) {
         });
         rect.on('mousedown', function () {
             model.color = index;
+            stage.draw();
             alert(JSON.stringify(model));
         });
         layer.add(rect);
     });
+
+    SBN.Service.ImageLoader.load(SBN.Contract.imageMap, function (images) {
+        $.each(SBN.Contract.content, function (index, value) {
+            var group = new Kinetic.Group();
+            var x = elementNumber++ * size;
+            var rect = new Kinetic.Rect({
+                x: x,
+                y: 0,
+                width: size,
+                height: size,
+                fill: '#555',
+                opacity: 0.65
+            });
+            group.add(rect);
+            var image = new Kinetic.Image({
+                x: x,
+                y: 0,
+                image: images[value] ? images[value] : images['e' + value],
+                width: size,
+                height: size
+            });
+            group.add(image);
+
+            group.on('mousedown', function () {
+                model.content = index;
+                model.isSelf = false;
+                stage.draw();
+                alert(JSON.stringify(model));
+            });
+            layer.add(group);
+            stage.draw();
+        });
+        $.each(SBN.Contract.content, function (index, value) {
+            var img = images['o' + value];
+            if (img) {
+                var group = new Kinetic.Group();
+                var x = elementNumber++ * size;
+                var rect = new Kinetic.Rect({
+                    x: x,
+                    y: 0,
+                    width: size,
+                    height: size,
+                    fill: '#555',
+                    opacity: 0.65
+                });
+                group.add(rect);
+                var image = new Kinetic.Image({
+                    x: x,
+                    y: 0,
+                    image: img,
+                    width: size,
+                    height: size
+                });
+                group.add(image);
+
+                group.on('mousedown', function () {
+                    model.content = index;
+                    model.isSelf = true;
+                    stage.draw();
+                    alert(JSON.stringify(model));
+                });
+                layer.add(group);
+                stage.draw();
+            }
+        });
+    });
+
     stage.add(layer);
 };
 
 SBN.Service = {};
-SBN.Service.Snake = {};
-SBN.Service.Snake.get = function (query, success) {
-    var url = '/api/Snake/Get' + (query ? '?' + $.param(query) : '');
-    $.ajax({
-        type: 'GET',
-        url: url,
-        contentType: "application/json",
-        dataType: 'json',
-        success: success || $.noop
-    });
+SBN.Service.ImageLoader = {
+    load: function (sources, callback) {
+        var images = {},
+         loaded = 0,
+         total = 0;
+        for (var source in sources) {
+            images[source] = new Image();
+            images[source].onload = function () {
+                if (++loaded >= total) {
+                    callback(images);
+                }
+            };
+            total++;
+        }
+        for (var src in sources) {
+            images[src].src = sources[src];
+        }
+    }
 };
-SBN.Service.Snake.save = function (snake, success) {
-    $.ajax({
-        type: 'POST',
-        url: '/api/Snake/Save',
-        contentType: "application/json",
-        dataType: 'json',
-        data: JSON.stringify(snake),
-        success: success || $.noop
-    });
+
+SBN.Service.Snake = {
+    get: function (query, success) {
+        var url = '/api/Snake/Get' + (query ? '?' + $.param(query) : '');
+        $.ajax({
+            type: 'GET',
+            url: url,
+            contentType: "application/json",
+            dataType: 'json',
+            success: success || $.noop
+        });
+    },
+    save: function (snake, success) {
+        $.ajax({
+            type: 'POST',
+            url: '/api/Snake/Save',
+            contentType: "application/json",
+            dataType: 'json',
+            data: JSON.stringify(snake),
+            success: success || $.noop
+        });
+    }
 };
