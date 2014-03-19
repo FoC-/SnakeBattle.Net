cb76a3f7434eb84bdb9fa5a960667ef22e31f43d
Nikolay Kush <nickola.kush@gmail.com>
2014-03-19 18:40:30 +0200

    Web: Select snakes for battle

--- src/SnakeBattleNet.Web/Controllers/ShowController.cs
+++ src/SnakeBattleNet.Web/Controllers/ShowController.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using AutoMapper;
+using SnakeBattleNet.Web.Core;
+using SnakeBattleNet.Web.Models.Snake;
 
 namespace SnakeBattleNet.Web.Controllers
 {
     public class ShowController : Controller
     {
+        private readonly ISnakeStore snakeStore;
+
+        public ShowController(ISnakeStore snakeStore)
+        {
+            this.snakeStore = snakeStore;
+        }
+
         public ActionResult Index()
         {
-            var model = new List<string>();
+            var snakes = snakeStore.GetAll();
+            var model = Mapper.Map<IEnumerable<Snake>, IEnumerable<SnakeViewModel>>(snakes);
             return View(model);
+        }
+
+        public ActionResult Go(IEnumerable<string> ids)
+        {
+            return View(ids);
         }
 
         public ActionResult Demo()
