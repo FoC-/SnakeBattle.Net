4ae422445e3ca1a98cf7749cf6abfef62a825ffe
Nikolay Kush <nickola.kush@gmail.com>
2014-05-26 15:14:25 +0300

    Update scripts and remove unobtrusive validation

--- src/SnakeBattleNet.Web/App_Start/Bootstrap.cs
+++ src/SnakeBattleNet.Web/App_Start/Bootstrap.cs
@@ -77,24 +77,19 @@ namespace SnakeBattleNet.Web
 
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"
+            bundles.Add(new ScriptBundle("~/bundles/jb").Include(
+                "~/js/jquery-{version}.js",
+                "~/js/bootstrap.js"
                 ));
 
-            bundles.Add(new ScriptBundle("~/bundles/kinetic").Include(
-                "~/Scripts/kinetic-v5.0.1.js"
+            bundles.Add(new ScriptBundle("~/bundles/ks").Include(
+                "~/js/kinetic-v5.1.0.js",
+                "~/js/site.js"
                 ));
 
-            bundles.Add(new ScriptBundle("~/bundles/site").Include(
-                "~/Scripts/site.js"
-                ));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                 "~/Content/bootstrap.css",
-                 "~/Content/Site.css"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                 "~/css/bootstrap.css",
+                 "~/css/site.css"));
         }
 
         public static void RegisterDependencies()
