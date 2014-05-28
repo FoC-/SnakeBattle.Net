62da29eab445cb724727eed4090bfa52b61f26ee
Nikolay Kush <nickola.kush@gmail.com>
2014-05-28 17:48:08 +0300

    Tests no comments

--- src/SnakeBattleNet.Test.Core/FieldComparerTests/When_call_DecidedDirections_with_predifined_chip_antother_tail.cs
+++ src/SnakeBattleNet.Test.Core/FieldComparerTests/When_call_DecidedDirections_with_predifined_chip_antother_tail.cs
@@ -9,19 +9,24 @@ namespace SnakeBattleNet.Test.Core.FieldComparerTests
     {
         Establish context = () =>
         {
-            fighter = CreateDummyFighter(10, 9).AttachChips(FullGreyWithOneEnemyTail());
-            var field = CreateFieldForFighters(new[] { fighter });
+            fighter = CreateDummyFighter(10, 8).Run(Direction.North).AttachChips(FullGreyWithOneEnemyTail());
+            var enemy1 = CreateDummyFighter(10, 10);
+            var enemy2 = CreateDummyFighter(11, 9).Run(Direction.North);
+            enemies = new[] { enemy1, enemy2 };
+
+            var field = CreateFieldForFighters(new[] { fighter, enemy1, enemy2 });
             comparer = CreateFieldComparer(field);
         };
 
         Because of = () =>
-            result = comparer.DecidedDirections(fighter, new[] { Direction.East, Direction.West, Direction.North, Direction.South, });
+            result = comparer.DecidedDirections(fighter, comparer.PossibleDirections(fighter, enemies));
 
         It should_return_move_on_that_row = () =>
-            result.ShouldContainOnly(new[] { Direction.North });
+            result.ShouldContainOnly(Direction.North, Direction.South);
 
         private static Direction[] result;
         private static Fighter fighter;
         private static FieldComparer comparer;
+        private static Fighter[] enemies;
     }
 }
