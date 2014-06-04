e59d8724f39db07b5cf8176b4c619dbd23fd3a05
Nikolay Kush <nickola.kush@gmail.com>
2014-06-04 16:02:30 +0300

    Emit

--- src/SnakeBattleNet.Web/js/site.js
+++ src/SnakeBattleNet.Web/js/site.js
@@ -541,3 +541,25 @@ SBN.App.set('Battle', SBN.Service.Resource.Battle);
 SBN.App.set('Snake', SBN.Service.Resource.Snake);
 SBN.App.set('ImageLoader', SBN.Service.ImageLoader);
 SBN.App.set('Renderer', SBN.Kinetic);
+
+SBN.AEM = new function () {
+    var listeners = {};
+
+    this.sub = function (name, func) {
+        listeners[name] = listeners[name] || [];
+        listeners[name][listeners[name].length] = func;
+    };
+
+    this.pub = function (e) {
+        var cbs = listeners[e.name];
+        if (cbs) {
+            for (var j = 0; j < cbs.length; j++) {
+                (function (f) {
+                    setTimeout(function () {
+                        f(e);
+                    }, 0);
+                })(cbs[j]);
+            }
+        }
+    }
+};
